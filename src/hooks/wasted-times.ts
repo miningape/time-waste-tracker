1185d64df79da7514a349705bc0e9b947ab0bf55
Kyle Johnson <miningape@gmail.com>
2024-06-14 17:04:19 +0200

    chore: update "database" with resolved date/time

--- src/hooks/wasted-times.ts
+++ src/hooks/wasted-times.ts
@@ -3,15 +3,21 @@ import utc from "dayjs/plugin/utc";
 
 dayjs.extend(utc);
 
-interface WastedTime {
-  status: "ongoing" | "resolved";
-  start: Dayjs;
-  end?: Dayjs;
-}
+type WastedTime =
+  | {
+      status: "ongoing";
+      start: Dayjs;
+    }
+  | {
+      status: "resolved";
+      start: Dayjs;
+      end: Dayjs;
+    };
 
 export const timeWasted: WastedTime[] = [
   {
-    status: "ongoing",
+    status: "resolved",
     start: dayjs("2024-06-14T09:00:00"),
+    end: dayjs("2024-06-14T16:50:00"),
   },
 ];
